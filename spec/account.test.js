808705271f4c2975ce1c511dcb4f244bf53bcd23
Ibrahim Pala <96876777+IbrahimPala1@users.noreply.github.com>
2022-07-27 09:28:42 +0100

    update on tests

--- spec/account.test.js
+++ spec/account.test.js
@@ -7,6 +7,14 @@ describe('Account', () => {
     it('checks to see that initial balance value is 0', () => {
         expect(account.balance).toEqual(0)
     })
+    it('deposits 10 to account and expects balance to be 10', () => {
+        account.deposit(10)
+        expect(account.balance).toEqual(10)
+    })
+    it('withdraws 10 from account and expects balance to be -10', () => {
+        account.withdraw(10)
+        expect(account.balance).toEqual(-10)
+    })
     it('checks to see 300 withdrawn ',() => {
         account.withdraw(300)
         expect(account.balance).toEqual(-300)
@@ -25,7 +33,7 @@ describe('Account', () => {
         account.deposit(200)
         expect(account.balance).toEqual(500)
     })
-    it('checks to see a mixture of withdraw and depopsit ',() => {
+    it('checks to see a mixture of withdraw and depopsit equals to the correct balance ',() => {
         account.deposit(200)
         account.withdraw(100)
         account.deposit(200)
@@ -36,26 +44,62 @@ describe('Account', () => {
         expect(account.date.length).toEqual(10)
     })
 })
-
-
-
-// describe('Date', () => {
-//     beforeEach = () => {
-//         const creatingDate = () => {
-//             var date = new Date();
-//             var dd = date.getDate();
-//             var mm = date.getMonth();
-//             var yy = date.getFullYear();
-          
-//             if(dd < 10) {
-//                  dd = '0' + dd
-//             }
-//             if(mm < 10) {
-//                  mm = '0' + mm
-//             }
-          
-//             date = dd + '/' + mm + '/' + yy
-//             return date
-//           }
-//     }
-// })
+describe('transactions array', () => {
+    beforeEach (() => {
+        account = new Account
+    })
+    it('checks transactions array has a length of 1', () => {
+        account.withdraw(100)
+        expect(account.transactions.length).toEqual(1)
+    })
+    it('checks transactions array has a length of 2', () => {
+        account.withdraw(100)
+        account.deposit(200)
+        expect(account.transactions.length).toEqual(2)
+    })
+    it('checks that the transactions array has a date', () => {
+        account.withdraw(20)
+        expect(account.transactions[0].date).toBeDefined()
+    })
+    it('checks that the transaction array has a balance', () => {
+        account.withdraw(20)
+        expect(account.transactions[0].balance).toBeDefined()
+    })
+    it('checks that the transaction array has a credit column', () => {
+        account.withdraw(20)
+        expect(account.transactions[0].credit).toBeDefined()
+    })
+    it('checks that the transaction array has debit column', () => {
+        account.withdraw(20)
+        expect(account.transactions[0].debit).toBeDefined()
+    })
+    it('checks that debit column is 20 when withdraw is 20 ', () => {
+        account.withdraw(20)
+        expect(account.transactions[0].debit).toEqual(20)
+    })
+    it('checks that credit column is empty when withdraw is 20 ', () => {
+        account.withdraw(20)
+        expect(account.transactions[0].credit).toEqual('')
+    })
+    it('checks that credit column is 20 when deposit is 20 ', () => {
+        account.deposit(20)
+        expect(account.transactions[0].debit).toEqual('')
+    })
+    it('checks that credit column is 20 when deposit is 20 ', () => {
+        account.deposit(20)
+        expect(account.transactions[0].credit).toEqual(20)
+    })
+    it('checks that balance column is 20 when deposit is 20 ', () => {
+        account.deposit(20)
+        expect(account.transactions[0].balance).toEqual(20)
+    })
+    it('checks that balance column is -20 when withdraw is 20 ', () => {
+        account.withdraw(20)
+        expect(account.transactions[0].balance).toEqual(-20)
+    })
+    it('checks that balance column is 5 when 10 is deposited and 5 is withdrawn ', () => {
+        account.deposit(10)
+        account.withdraw(5)
+        expect(account.transactions[1].balance).toEqual(5)
+    })
+})
--- spec/transaction.test.js
+++ spec/transaction.test.js
@@ -1,15 +1,34 @@
 const { Transaction } = require('../lib/app')
 describe('transaction', () => {
-
-    it('checks for debit balance', () => {
+    it('checks that balance is defined', () => {
+        transaction = new Transaction('debit', 10, 10)
+        expect(transaction.balance).toBeDefined()
+    })
+    it('checks that debit for debit type is defined', () => {
+        transaction = new Transaction('debit', 10, 10)
+        expect(transaction.debit).toBeDefined()
+    })
+    it('checks that credit for debit type does not work', () => {
+        transaction = new Transaction('debit', 10, 10)
+        expect(transaction.credit).toEqual('')
+    })
+    it('checks that credit is defined', () => {
+        transaction = new Transaction('credit', 10, 10)
+        expect(transaction.credit).toBeDefined()
+    })
+    it('checks balance after debit type', () => {
         transaction = new Transaction('debit', 10, 10)
         expect(transaction.balance).toEqual(10)
+    })
+    it('checks that debit for credit type does not work', () => {
+        transaction = new Transaction('credit', 10, 10)
+        expect(transaction.debit).toEqual('')
     })
     it('checks for debit amount', () => {
         transaction = new Transaction('debit', 10, 10)
         expect(transaction.debit).toEqual(10)
     })
-    it('checks for credit balance', () => {
+    it('checks balance after credit type', () => {
         transaction = new Transaction('credit', 10, 10)
         expect(transaction.balance).toEqual(10)
     })
